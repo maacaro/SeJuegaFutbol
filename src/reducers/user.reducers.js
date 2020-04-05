45206891958057bc8d074d91068b9bf88886f1b3
Manuel Castro <maacaro@gmail.com>
2020-04-05 16:14:33 -0400

    SJF-009 creae user reducer for authentication

---
+++ src/reducers/user.reducers.js
@@ -0,0 +1,53 @@
+export {user};
+
+const userDefaultState = {
+  isLoading: false,
+  isSignIn: false,
+  userToken: null,
+};
+
+function user(prevState = userDefaultState, action = {}) {
+  switch (action.type) {
+    case 'SUCCESS_RESTORE_TOKEN':
+      return {
+        ...prevState,
+        userToken: action.token,
+        isSignIn: true,
+      };
+    case 'FAILURE_RESTORE_TOKEN':
+      return {
+        ...prevState,
+        isSignIn: false,
+      };
+    case 'SUCCESS_SIGN_IN':
+      return {
+        ...prevState,
+        isSignIn: true,
+        userToken: action.token,
+      };
+    case 'FAILURE_SIGN_IN':
+      return {
+        ...prevState,
+        isSignIn: false,
+        userToken: null,
+      };
+    case 'SUCCESS_SIGN_OUT':
+      return {
+        ...prevState,
+        isSignIn: false,
+        userToken: null,
+      };
+    case 'AUTH_START':
+      return {
+        ...prevState,
+        isLoading: true,
+      };
+    case 'AUTH_ENDS':
+      return {
+        ...prevState,
+        isLoading: false,
+      };
+    default:
+      return prevState;
+  }
+}
